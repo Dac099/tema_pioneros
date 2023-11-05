56854192e8f446bd348dac84c19a284213efe32a
Dac099 <davidcejazapata@gmail.com>
2023-11-05 11:32:45 -0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './tamplates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
